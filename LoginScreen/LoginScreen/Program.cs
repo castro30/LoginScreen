5a6aa085cfd76d15b74a39e517a762b6d200e3e7
castro30 <castro30@yahoo.com>
2017-03-25 23:42:07 -0500

    First Draft

---
+++ LoginScreen/LoginScreen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // login screen that accepts login name and password. 
+            // Allow the user a maximum of 3 chances only. 
+            // In case the user is unsuccessful in logging in after three attempts
+            // exit the application.
+            Console.Write("Enter login: ");
+            string LogIn = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string passWord = Console.ReadLine();
+
+           
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
