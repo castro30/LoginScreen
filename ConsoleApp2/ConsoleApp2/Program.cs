e35c49f272bc5e364cb0e1ee14a6284ad0108aa1
castro30 <castro30@yahoo.com>
2017-05-21 13:17:55 -0500

    Update

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1, i2, i3;
+            Console.WriteLine("Enter i3: ");
+            string newi3 = Console.ReadLine();
+            i3 = Convert.ToInt32(newi3);
+
+i2 = 0;
+            for (int i4 = 0; i4 > i3; i4++)
+            { 
+                
+                Console.WriteLine("Enter i1: ");
+                string newi1 = Console.ReadLine();
+                i1 = Convert.ToInt32(newi1);
+                i2 = i2 + i1;
+                Console.WriteLine("Sum = {0}", i2);
+Console.ReadKey();
+            }
+        }
+
+    }
+}
